e665808f2b5652d4e2acedcf32dde5ca30e5e8f8
OlBreeze <fmwmf@ukr.net>
2024-11-13 23:41:07 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./component/Header";
+import {useState} from "react";
+import {haverim} from "./utils/constants";
+import Main from "./component/Main";
+import {StarWarsContext} from "./utils/StarWarsContext";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+
+const App = () => {
+    const [mainHero, setMainHero]     = useState(haverim.luke);
+    const [gallery, setGallery]       = useState(Object.values(haverim).filter(img => img !== haverim.luke));
+
+    const handleImageClick = (clickedImage) => {
+        setGallery(prevGallery => {
+            const updatedGallery = prevGallery.map(img => img === clickedImage ? mainHero : img);
+            setMainHero(clickedImage); // Меняем главную картинку на кликнутую
+            return updatedGallery;
+        });
+    };
+    return (
+        <div className="mainBox container-fluid">
+            <StarWarsContext.Provider value={{
+                mainHero, gallery, setMainHero,
+                handleImageClick
+            }}>
+                <Header/>
+                <Main/>
+            </StarWarsContext.Provider>
+        </div>
+    )
+        ;
+};
 
 export default App;
+
+
+
+
